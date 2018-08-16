2b36149683972767fb5ea69374e4ac10762a999e
Amy Zhou <aymeighz@gmail.com>
2018-08-16 22:38:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+ if array.length == 1
+   array.join 
+ elsif array.length == 2 
+  array.join(" and ")
+ elsif array.length == 3 
+  array << "and #{array.pop}"
+  array.join(", ")
+else 
+  array << "and #{array.pop}"
+  array.join(", ") 
 end
+end
+
+
